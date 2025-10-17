f1d7a6e26546d40c5f4580aa48420cc6bb4ddb40
jackdevitt <jacktdevitt@gmail.com>
2025-10-17 11:55:26 -0400

    Finished leaderboard implementation

---
+++ binaryHelper.js
@@ -0,0 +1,7 @@
+function intToBin(num) {
+    return num.toString(2);
+}
+
+function binToInt(bin) {
+    return parseInt(bin, 2);
+}
--- leaderboard.js
+++ leaderboard.js
@@ -1,17 +1,18 @@
-const TOKEN = "lpwnhkdchqvxe";
-const DECK_COLUMN_ID = "144411";
+const TOKEN = "dqhzyddctffje";
+const SCORE_ID = "144730";
+const DECK_ID = "144731";
 const id = localStorage.getItem("id");
 
 function getBoardData() {
-    fetch(`https://leaderboarded.com/api/${TOKEN}/board/`)
+    return fetch(`https://leaderboarded.com/api/${TOKEN}/board/`)
     .then(response => {
         if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
         return response.json();
     })
     .then(data => {
-        console.log('API response:', data);
+        return data;
     })
     .catch(error => {
         console.error('Error fetching data:', error);
@@ -42,48 +43,7 @@ function addPlayer(name) {
     .catch(error => console.error('Error:', error));
 }
 
-function setPlayerScore(id, scoreToSet) {
-    if (id == undefined) {
-        console.error("Player not yet initialized");
-        return;
-    }
-    fetch(`https://leaderboarded.com/api/${TOKEN}/score/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            player_id: `${id}`,
-            score: scoreToSet,
-            operation: "set"
-        }),
-    })
-    .catch(error => console.error('Error:', error));
-}
-
-function createColumn() {
-    fetch(`https://leaderboarded.com/api/${TOKEN}/column/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            name: "decks",
-            sorting: "ASC",
-            sort_priority: 0,
-            prefix: "",
-            suffix: "",
-            format: "INTEGER",
-            increments: [
-                "1"
-            ]
-        }),
-    })
-    .then(response => console.log(response.json()))
-    .catch(error => console.error('Error:', error))
-}
-
-function setDeckScore(id, scoreToSet) {
+function setPlayerScore(scoreToSet) {
     if (id == undefined) {
         console.error("Player not yet initialized");
         return;
@@ -94,11 +54,87 @@ function setDeckScore(id, scoreToSet) {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            column_id: DECK_COLUMN_ID,
+            column_id: SCORE_ID,
             player_id: `${id}`,
             score: scoreToSet,
             operation: "set"
         }),
     })
     .catch(error => console.error('Error:', error));
-}   
+}
+
+function setDeckScore(scoreToSet) {
+    if (id == undefined) {
+        console.error("Player not yet initialized");
+        return;
+    }
+    fetch(`https://leaderboarded.com/api/${TOKEN}/multiscore/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            column_id: DECK_ID,
+            player_id: `${id}`,
+            score: scoreToSet,
+            operation: "set"
+        }),
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function handleDeckUpdate() {
+    //GET BINARY REPRESENTING DECKS
+    //
+    //Example: You have decks in the following order:
+    //Blue (yes), Red (no), Green (no)
+    // Return: 1 0 0
+    let bin = 100;
+    let num = binToInt(bin);
+    setDeckScore(num);
+}
+
+function getPlayersInOrder() {
+    getBoardData().then(data => {
+        let players = [];
+        for (let i = 0; i < data.players.length; i++) {
+            const player = data.players[i];
+            const parsedPlayer = {
+                name: player.name,
+                score: player.scores[SCORE_ID].score
+            };
+            players.push(parsedPlayer);
+        }
+        let sortedPlayers = data.sort((a, b) => a.score - b.score);
+        return sortedPlayers;
+    });
+}
+
+function getInitalData() {
+    if (id == undefined) {
+        return "Player Setup";
+    }
+    getBoardData().then(data => {
+        for (let i = 0; i < data.players.length; i++) {
+            const player = data.players[i];
+            if (player.id == id) {
+                let parsedPlayer = {
+                    name: player.name,
+                    score: player.scores[SCORE_ID].score,
+                    decks: player.scores[DECK_ID].score
+                }
+                return parsedPlayer;
+            }
+        }
+        console.error("ID not found in leaderboard service");
+        return;
+    });
+}
+
+function clearId() {
+    localStorage.removeItem("id");
+}
+
+function setId(id) {
+    localStorage.setItem("id", id);
+}
