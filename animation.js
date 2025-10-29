ab3168cdf2b5cde362961cf2c8cd123fadaef9ee
Griffin <moran2gc@dukes.jmu.edu>
2025-10-29 10:57:56 -0400

    animation helper 90% complete

--- animation.js
+++ animation.js
@@ -1,12 +1,24 @@
+let isAnimated = false;
+
 const deck = document.getElementById('dealer-deck');
 const target = document.getElementById('playerC1');
 const dealButton = document.getElementById('deal-button');
 
 dealButton.addEventListener("click", () => {
+  if (isAnimated){
+    return;
+  }
+  
   const deckRect = deck.getBoundingClientRect();
   const targetRect = target.getBoundingClientRect();
 
-  const deltaX = targetRect.left - deckRect.left;
-  const deltaY = targetRect.top - deckRect.top;
+  const targetCenterX = targetRect.left + (targetRect.width / 2);
+  const targetCenterY = targetRect.top + (targetRect.height / 2);
+  const deckCenterX = deckRect.left + (deckRect.width / 2);
+  const deckCenterY = deckRect.top + (deckRect.height / 2);
+  const deltaX = targetCenterX - deckCenterX;
+  const deltaY = targetCenterY - deckCenterY;
+
   deck.style.transform = `translate(${deltaX}px, ${deltaY}px) rotate(180deg)`;
+  isAnimated = true;
 });
