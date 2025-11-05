f7f05e213c7207917d40ca1b1a143b5db5fd054c
Griffin <moran2gc@dukes.jmu.edu>
2025-11-05 10:35:51 -0500

    renamed card file, card visuals almost done, started card deck api work

---
+++ cardsVisual.js
@@ -0,0 +1,278 @@
+// EVENTUALLY CHANGE VW/VH TO %
+let testCard = document.getElementById("playerC1");
+
+function setupCard(card){
+  ace(card);
+}
+
+function ace(card){
+  let imgs = [];
+  let img1 = document.createElement("img");
+  imgs.push(img1);
+  card.appendChild(imgs[0]);
+  img1.style.paddingTop = '3vh';
+  img1.style.width = '75%';
+  heart(imgs);
+}
+
+function two(card){
+  let imgs = [];
+  let img1 = document.createElement("img");
+  imgs.push(img1);
+  let img2 = document.createElement("img");
+  imgs.push(img2);
+
+  for (let img of imgs){
+    card.appendChild(img);
+    img.style.paddingTop = '1vh';
+    img.style.width = '55%';
+  }
+}
+
+function three(card){
+  let imgs = [];
+  let img1 = document.createElement("img");
+  imgs.push(img1);
+  let img2 = document.createElement("img");
+  imgs.push(img2);
+  let img3 = document.createElement("img");
+  imgs.push(img3);
+
+  for (let img of imgs){
+    card.appendChild(img);
+    img.style.paddingTop = '.2vh';
+    img.style.paddingLeft = '.5vw';
+    img.style.paddingRight = '.5vw';
+    img.style.width = '43%';
+  }
+}
+
+function four(card){
+  let imgs = [];
+  let img1 = document.createElement("img");
+  imgs.push(img1);
+  let img2 = document.createElement("img");
+  imgs.push(img2);
+  let img3 = document.createElement("img");
+  imgs.push(img3);
+  let img4 = document.createElement("img");
+  imgs.push(img4);
+
+  for (let img of imgs){
+    card.appendChild(img);
+    img.style.paddingTop = '2vh';
+    img.style.paddingLeft = '.2vw';
+    img.style.paddingRight = '.2vw';
+    img.style.width = '30%';
+  }
+}
+
+function five(card){
+  let imgs = [];
+  let img1 = document.createElement("img");
+  imgs.push(img1);
+  let img2 = document.createElement("img");
+  imgs.push(img2);
+  let img3 = document.createElement("img");
+  imgs.push(img3);
+  let img4 = document.createElement("img");
+  imgs.push(img4);
+  let img5 = document.createElement("img");
+  imgs.push(img5);
+
+  for (let img of imgs){
+    card.appendChild(img);
+    img.style.paddingTop = '1vh';
+    img.style.paddingLeft = '.2vw';
+    img.style.paddingRight = '.2vw';
+    img.style.width = '28%';
+  }
+}
+
+function six(card){
+  let imgs = [];
+  let img1 = document.createElement("img");
+  imgs.push(img1);
+  let img2 = document.createElement("img");
+  imgs.push(img2);
+  let img3 = document.createElement("img");
+  imgs.push(img3);
+  let img4 = document.createElement("img");
+  imgs.push(img4);
+  let img5 = document.createElement("img");
+  imgs.push(img5);
+  let img6 = document.createElement("img");
+  imgs.push(img6);
+
+  for (let img of imgs){
+    card.appendChild(img);
+    img.style.paddingTop = '1vh';
+    img.style.paddingLeft = '.3vw';
+    img.style.paddingRight = '.3vw';
+    img.style.width = '25%';
+  }
+}
+
+function seven(card){
+  let imgs = [];
+  let img1 = document.createElement("img");
+  imgs.push(img1);
+  let img2 = document.createElement("img");
+  imgs.push(img2);
+  let img3 = document.createElement("img");
+  imgs.push(img3);
+  let img4 = document.createElement("img");
+  imgs.push(img4);
+  let img5 = document.createElement("img");
+  imgs.push(img5);
+  let img6 = document.createElement("img");
+  imgs.push(img6);
+  let img7 = document.createElement("img");
+  imgs.push(img7);
+
+  for (let img of imgs){
+    card.appendChild(img);
+    img.style.paddingTop = '.8vh';
+    img.style.paddingLeft = '.3vw';
+    img.style.paddingRight = '.3vw';
+    img.style.width = '20%';
+  }
+}
+
+function eight(card){
+  let imgs = [];
+  let img1 = document.createElement("img");
+  imgs.push(img1);
+  let img2 = document.createElement("img");
+  imgs.push(img2);
+  let img3 = document.createElement("img");
+  imgs.push(img3);
+  let img4 = document.createElement("img");
+  imgs.push(img4);
+  let img5 = document.createElement("img");
+  imgs.push(img5);
+  let img6 = document.createElement("img");
+  imgs.push(img6);
+  let img7 = document.createElement("img");
+  imgs.push(img7);
+  let img8 = document.createElement("img");
+  imgs.push(img8);
+
+  for (let img of imgs){
+    card.appendChild(img);
+    img.style.paddingTop = '.8vh';
+    img.style.paddingLeft = '.3vw';
+    img.style.paddingRight = '.3vw';
+    img.style.width = '20%';
+  }
+}
+
+function nine(card){
+  let imgs = [];
+  let img1 = document.createElement("img");
+  imgs.push(img1);
+  let img2 = document.createElement("img");
+  imgs.push(img2);
+  let img3 = document.createElement("img");
+  imgs.push(img3);
+  let img4 = document.createElement("img");
+  imgs.push(img4);
+  let img5 = document.createElement("img");
+  imgs.push(img5);
+  let img6 = document.createElement("img");
+  imgs.push(img6);
+  let img7 = document.createElement("img");
+  imgs.push(img7);
+  let img8 = document.createElement("img");
+  imgs.push(img8);
+  let img9 = document.createElement("img");
+  imgs.push(img9);
+
+  for (let img of imgs){
+    card.appendChild(img);
+    img.style.paddingTop = '.2vh';
+    img.style.paddingLeft = '.3vw';
+    img.style.paddingRight = '.3vw';
+    img.style.width = '19%';
+  }
+  // Need space above cards, alignment unclean otherwise
+  img2.style.paddingTop = '.7vh';
+}
+
+function ten(card){
+  let imgs = [];
+  let img1 = document.createElement("img");
+  imgs.push(img1);
+  let img2 = document.createElement("img");
+  imgs.push(img2);
+  let img3 = document.createElement("img");
+  imgs.push(img3);
+  let img4 = document.createElement("img");
+  imgs.push(img4);
+  let img5 = document.createElement("img");
+  imgs.push(img5);
+  let img6 = document.createElement("img");
+  imgs.push(img6);
+  let img7 = document.createElement("img");
+  imgs.push(img7);
+  let img8 = document.createElement("img");
+  imgs.push(img8);
+  let img9 = document.createElement("img");
+  imgs.push(img9);
+  let img10 = document.createElement("img");
+  imgs.push(img10);
+
+  for (let img of imgs){
+    card.appendChild(img);
+    img.style.paddingTop = '.4vh';
+    img.style.width = '19%';
+  }
+  // Double digits require special alignment
+  // Maybe shrink letter spacing
+  img1.style.paddingLeft = '.8vw';
+  img1.style.paddingRight = '.2vw';
+  img2.style.paddingRight = '.7vw';
+
+  img3.style.paddingLeft = '.8vw';
+  img3.style.paddingRight = '.2vw';
+  img4.style.paddingRight = '.8vw';
+
+  img5.style.paddingLeft = '.8vw';
+  img5.style.paddingRight = '.2vw';
+  img6.style.paddingRight = '.8vw';
+
+  img7.style.paddingLeft = '.8vw';
+  img7.style.paddingRight = '.2vw';
+  img8.style.paddingRight = '.8vw';
+
+  img9.style.paddingLeft = '.8vw';
+  img9.style.paddingRight = '.2vw';
+  img10.style.paddingRight = '.8vw';
+}
+
+function heart(imgs){
+  for (let img of imgs){
+    img.src = "card_imgs/heart.png";
+  }
+}
+
+function diamond(imgs){
+  for (let img of imgs){
+    img.src = "card_imgs/diamond.png";
+  }
+}
+
+// need new spade image
+function spade(imgs){
+  for (let img of imgs){
+    img.src = "card_imgs/spade.png";
+  }
+}
+
+function club(imgs){
+  for (let img of imgs){
+    img.src = "card_imgs/club.png";
+  }
+}
+
+setupCard(testCard);
---
+++ deck.js
@@ -0,0 +1,10 @@
+const hit = document.getElementById("hit-button");
+const stand = document.getElementById("stand-button");
+const deal = document.getElementById("deal-button");
+const split = document.getElementById("split-button");
+const double = document.getElementById("double-button");
+
+let deckId = null;
+const shuffleDeck = async () => {
+  
+}
