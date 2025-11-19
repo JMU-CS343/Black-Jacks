443729a3c05988e57d73af0a242ae78123106c9b
jackdevitt <jacktdevitt@gmail.com>
2025-11-18 22:42:47 -0500

    Made some changes

--- profile.js
+++ profile.js
@@ -5,8 +5,11 @@ const playerIdField = document.getElementById("playerId");
 const signOut = document.getElementById("signOut");
 const idInput = document.getElementById("idInput");
 const warning = document.getElementById("warning");
+const invalidIdWarning = document.getElementById("invalidIdWarning");
+const alternative = document.getElementById("alternative");
+const createAccount = document.getElementById("createAccount");
 
-const id = localStorage.getItem("id");
+let id = localStorage.getItem("id");
 const TOKEN = "dqhzyddctffje";
 const SCORE_ID = "144730";
 const DECK_ID = "144731";
@@ -75,8 +78,10 @@ function init() {
             decksObtainedField.textContent = `Decks collected: ${deckCount}/${maxDecks}`
             playerIdField.textContent = `id: ${localStorage.getItem("id")}`;
 
-            warning.style = "display:block;";
+            warning.style = "display:block;color:red;";
             idInput.style = "display:none;";
+            createAccount.style = "display:none;"
+            alternative.style="display:none;"
         });
     } else {
         usernameField.textContent = "Please enter your ID to sign in"
@@ -84,16 +89,115 @@ function init() {
 
         warning.style = "display:none;";
         idInput.style = "display:block;";
+        createAccount.style = "display:block;margin:auto;"
+        alternative.style="display:block;"
     }
 }
 init();
 
+function addPlayer(name) {
+    fetch(`https://leaderboarded.com/api/${TOKEN}/player/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            is_eliminated: false,
+            is_visible: true,
+            text_color: "#FFFFFFFF",
+            background_color: "FFC800FF",
+            team: null,
+            score: 0,
+            
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        localStorage.setItem("id", data.player.id);
+        id = localStorage.getItem("id");
+        setDeckScore(1);
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function verifyId(id) {
+  return getBoardData().then(data => {
+    for (let i = 0; i < data.players.length; i++) {
+        const player = data.players[i];
+        console.log(player.id + ", " + id);
+        if (player.id == id) {
+            return true;
+        }
+    }
+    return false;
+  });
+}
+
 signOut.addEventListener("click", () => {
     if (localStorage.getItem("id") != undefined) {
         localStorage.removeItem("id");
+        window.location.href = "profile.html";
     } else {
-        localStorage.setItem("id", idInput.value);
-        console.log(idInput.value);
+        let id = idInput.value;
+        console.log(id);
+        verifyId(id).then(value => {
+            if (value) {
+                localStorage.setItem("id", id);
+                window.location.href = "profile.html";
+            } else {
+                invalidIdWarning.style = "display:block;color:red;"
+                invalidIdWarning.textContent = `\"${id}\" is not a valid ID`;
+            }
+        });
     }
-    window.location.href = "index.html";
+});
+
+createAccount.addEventListener("click", () => {
+    console.log("CLUH");
+    const blur = document.createElement("div");
+    blur.setAttribute("class", "blur");
+
+    const popup = document.createElement("div");
+    popup.setAttribute("class", "popup");
+
+    const header = document.createElement("h3");
+    header.setAttribute("class", "popupTitle");
+    header.textContent = "Enter a username";
+
+    const inputField = document.createElement("input");
+    inputField.setAttribute("type", "text");
+    inputField.setAttribute("placeholder", "username");
+
+    const buttons = document.createElement("div");
+    buttons.style="display:flex;justify-content:space-evenly;margin-top:25px;width:30%;";
+
+    const submit = document.createElement("button");
+    submit.textContent = "Submit";
+    submit.addEventListener("click", () => {
+
+        let name = inputField.value;
+        addPlayer(name);
+
+        document.body.removeChild(blur);
+        document.body.removeChild(popup);
+
+        //window.location.href = "profile.html";
+    });
+
+    const cancel = document.createElement("button");
+    cancel.textContent = "Cancel";
+    cancel.addEventListener("click", () => {
+        document.body.removeChild(blur);
+        document.body.removeChild(popup);
+    });
+
+    buttons.appendChild(submit);
+    buttons.appendChild(cancel);
+
+    popup.appendChild(header);
+    popup.appendChild(inputField);
+    popup.appendChild(buttons);
+    document.body.appendChild(blur);
+    document.body.appendChild(popup);
 });
