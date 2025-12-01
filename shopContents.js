3d7cc645eb1571a84a0062fc3351dad83a216815
jackdevitt <jacktdevitt@gmail.com>
2025-12-01 11:14:44 -0500

    Added last three cards

---
+++ shopContents.js
@@ -0,0 +1,68 @@
+const decks = [
+    {
+        "title": "default",
+        "rarity": "common"
+    },
+    {
+        "title": "blue",
+        "rarity": "common"
+    },
+    {
+        "title": "red",
+        "rarity": "common"
+    },
+    {
+        "title": "green",
+        "rarity": "common"
+    },
+    {
+        "title": "yellow",
+        "rarity": "common"
+    },
+    {
+        "title": "white",
+        "rarity": "common"
+    },
+    {
+        "title": "purple",
+        "rarity": "uncommon"
+    },
+    {
+        "title": "maroon",
+        "rarity": "uncommon"
+    },
+    {
+        "title": "hot pink",
+        "rarity": "uncommon"
+    },
+    {
+        "title": "eggshell",
+        "rarity": "uncommon"
+    },
+    {
+        "title": "chromatic",
+        "rarity": "rare"
+    },
+    {
+        "title": "metallic",
+        "rarity": "rare"
+    },
+    {
+        "title": "glass",
+        "rarity": "rare"
+    },
+    {
+        "title": "jmu",
+        "rarity": "epic"
+    },
+    {
+        "title": "golden",
+        "rarity": "epic"
+    },
+    {
+        "title": "custom",
+        "rarity": "legendary"
+    },
+]
+
+const costs = {"common": "$250", "uncommon": "$500", "rare": "$2000", "epic": "$5000", "legendary": "$15000"};
